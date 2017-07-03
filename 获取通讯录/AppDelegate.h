c70827cee636c3cbf110fb8180640aed85c3a071
Miao123 <miao_ios@163.comc>
2017-07-03 16:07:06 +0800

    Initial Commit

---
+++ 获取通讯录/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  获取通讯录
+//
+//  Created by 苗建浩 on 2017/7/3.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 获取通讯录/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  获取通讯录
+//
+//  Created by 苗建浩 on 2017/7/3.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
